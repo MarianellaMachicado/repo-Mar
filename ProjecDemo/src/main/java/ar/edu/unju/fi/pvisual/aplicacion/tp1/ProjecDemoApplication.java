77da31f7c0657771c3ef284509e3d8fadb17d77c
MarianellaMachicado <machicadomarianella@gmail.com>
2022-04-11 18:21:37 -0300

    Primer commit

---
+++ ProjecDemo/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/tp1/ProjecDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion.tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecDemoApplication.class, args);
+	}
+
+}
---
+++ ProjecDemo/src/test/java/ar/edu/unju/fi/pvisual/aplicacion/tp1/ProjecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion.tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
