e67e905bbc4f286aad3f8b9e186dcafc91d6c1b7
MarianellaMachicado <machicadomarianella@gmail.com>
2022-04-11 22:48:29 -0300

    Cree la clase Autor. Agregre atributos, constructores y metodos geters y seters a las clases Autor y Libro.

---
+++ ProjecDemo/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/tp1/model/Autor.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.pvisual.aplicacion.tp1.model;
+
+public class Autor {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public Autor(){
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}	
+}
--- ProjecDemo/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/tp1/model/Libro.java
+++ ProjecDemo/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/tp1/model/Libro.java
@@ -1,5 +1,40 @@
 package ar.edu.unju.fi.pvisual.aplicacion.tp1.model;
 
 public class Libro {
+	private String nombre;
+	private Autor autor;
+	private String genero;
+	
+	public Libro(String nombre, Autor autor, String genero) {
+		super();
+		this.nombre = nombre;
+		this.autor = autor;
+		this.genero = genero;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
 }
