5e5d3fa297f65a60deb50a18f267de0a399d22ed
MarianellaMachicado <machicadomarianella@gmail.com>
2022-05-30 01:09:57 -0300

    Base para el proyecto tp5

---
+++ TP5_Marianella/src/test/java/pvisual/tp5/Tp5MarianellaApplicationTests.java
@@ -0,0 +1,13 @@
+package pvisual.tp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5MarianellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
